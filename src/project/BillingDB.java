2a000371f3e43375d18ca46920c23508c9cecd57
PhaZer1604 <vidhyapatishelke999@gmail.com>
2023-03-13 00:45:39 +0530

    initial commit

---
+++ src/project/BillingDB.java
@@ -0,0 +1,58 @@
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class BillingDB {
+    
+    public static void insertIntoBillingDB(String username, int price, String date){
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothingdb","admin","");
+            PreparedStatement ps = con.prepareStatement("INSERT INTO billing(uname, bill, date) VALUES(?,?,?)");
+            ps.setString(1, username);
+            ps.setInt(2, price);
+            ps.setString(3, date);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(WomenDB.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
+    
+    public static ArrayList<BillObject> billlings(){
+        ArrayList<BillObject> customers = new ArrayList<>();     
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothingdb","admin","");
+            Statement ps = con.createStatement();
+            ResultSet rs = ps.executeQuery("SELECT id, uname, bill, date FROM billing");           
+            BillObject pl;           
+            while(rs.next()){
+                pl = new BillObject(rs.getInt("id"),rs.getString("uname"),
+                        rs.getInt("bill"),rs.getString("date"));
+                customers.add(pl);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(WomenDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return customers;
+    }
+
+    public static void deleteBillings(){
+         try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothingdb","admin","");
+            PreparedStatement ps = con.prepareStatement("DELETE FROM billing");
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "All entries have been deleted!");
+        } catch (SQLException ex) {
+            Logger.getLogger(MenDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }   
+}
